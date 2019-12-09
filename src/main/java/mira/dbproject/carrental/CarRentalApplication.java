7f4ca823d487c8f9a961db3fcb57e6d9d0d8e693
michal.wiercinski <michal.wiercinski@int.pl>
2019-12-09 19:14:26 +0100

    Initial commit

---
+++ src/main/java/mira/dbproject/carrental/CarRentalApplication.java
@@ -0,0 +1,13 @@
+package mira.dbproject.carrental;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CarRentalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CarRentalApplication.class, args);
+	}
+
+}
